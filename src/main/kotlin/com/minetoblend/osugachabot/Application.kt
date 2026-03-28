7d46305fe92c85698812e1307c77f3c063c30deb
marvin <m.schuerz@hautzy.com>
2026-03-28 02:31:16 +0100

    rename main application class

--- src/main/kotlin/com/minetoblend/osugachabot/Application.kt
+++ src/main/kotlin/com/minetoblend/osugachabot/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class OsugachabotApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<OsugachabotApplication>(*args)
+    runApplication<Application>(*args)
 }
