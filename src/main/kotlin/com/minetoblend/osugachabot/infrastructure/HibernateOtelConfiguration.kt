413c118dce8a7404f2111daae4d393cbf3602e83
marvin <m.schuerz@hautzy.com>
2026-03-29 23:27:24 +0200

    fix otel warnings on server start

--- src/main/kotlin/com/minetoblend/osugachabot/infrastructure/HibernateOtelConfiguration.kt
+++ src/main/kotlin/com/minetoblend/osugachabot/infrastructure/HibernateOtelConfiguration.kt
@@ -2,20 +2,30 @@ package com.minetoblend.osugachabot.infrastructure
 
 import io.opentelemetry.api.OpenTelemetry
 import io.opentelemetry.instrumentation.jdbc.datasource.JdbcTelemetry
+import org.springframework.beans.factory.ObjectProvider
 import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Role
+import org.springframework.beans.factory.config.BeanDefinition
 import javax.sql.DataSource
 
 @Configuration
 class HibernateOtelConfiguration {
 
-    @Bean
-    fun jdbcOtelBeanPostProcessor(openTelemetry: OpenTelemetry): BeanPostProcessor {
-        val telemetry = JdbcTelemetry.builder(openTelemetry).build()
-        return object : BeanPostProcessor {
-            override fun postProcessAfterInitialization(bean: Any, beanName: String): Any =
-                if (bean is DataSource) telemetry.wrap(bean) else bean
+    companion object {
+        @JvmStatic
+        @Bean
+        @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+        fun jdbcOtelBeanPostProcessor(openTelemetryProvider: ObjectProvider<OpenTelemetry>): BeanPostProcessor {
+            return object : BeanPostProcessor {
+                private val telemetry by lazy {
+                    JdbcTelemetry.builder(openTelemetryProvider.getObject()).build()
+                }
+
+                override fun postProcessAfterInitialization(bean: Any, beanName: String): Any =
+                    if (bean is DataSource) telemetry.wrap(bean) else bean
+            }
         }
     }
 }
